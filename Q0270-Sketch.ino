cc8f633f9e9b3a5688077e71d3bf9c89b9fb0f6b
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2020-03-22 14:11:26 -0300

    Automação Residencial - Inicial

---
+++ Q0270-Sketch.ino
@@ -0,0 +1,30 @@
+#define pinRele 5
+
+#include <SoftwareSerial.h>
+SoftwareSerial btSerial(8, 9); // RX, TX
+
+void setup() {
+  pinMode(pinRele, OUTPUT);
+  digitalWrite(pinRele, HIGH);
+  btSerial.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+static String btComando;
+static bool releStatus = HIGH; 
+
+  while (btSerial.available()) {
+     char comandoRec = btSerial.read();
+     btComando += char( comandoRec );
+     Serial.print(comandoRec);
+
+     if (comandoRec == ';') {
+        if (btComando == "B1;") {
+            releStatus = !releStatus;
+            digitalWrite(pinRele, releStatus);
+        }
+        btComando = ""; 
+     }
+  }
+}
